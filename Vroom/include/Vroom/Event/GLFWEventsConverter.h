a7daa6d2c6d9cea7e1f4be9ecf3fd79eecb8a84b
Alexandre <beaujonalexandre@gmail.com>
2024-02-18 23:24:07 +0100

    Documentation.

--- Vroom/include/Vroom/Event/GLFWEventsConverter.h
+++ Vroom/include/Vroom/Event/GLFWEventsConverter.h
@@ -10,12 +10,39 @@ namespace vrm
 class GLFWEventsConverter
 {
 public:
+
+    /**
+     * @brief Will initialize static mappers of event codes.
+     * Has to be called before using conversion functions.
+     */
     static void Init();
 
+    /**
+     * @brief Get Vroom key code from a GLFW key code.
+     * @param GLFWCode GLFW key code.
+     * @return Vroom key code.
+     */
     static KeyCode GetKeyCodeFromGLFW(int GLFWCode);
+
+    /**
+     * @brief Get Vroom mouse code from a GLFW mouse code.
+     * @param GLFWCode GLFW mouse code.
+     * @return Vroom mouse code.
+     */
     static MouseCode GetMouseCodeFromGLFW(int GLFWCode);
 
+    /**
+     * @brief Get GLFW key code from a Vroom key code.
+     * @param code Vroom key code.
+     * @return GLFW key code.
+     */
     static int GetGLFWFromKeyCode(const KeyCode& code);
+
+    /**
+     * @brief Get GLFW mouse code from a Vroom key code.
+     * @param code Vroom key code.
+     * @return GLFW mouse code.
+     */
     static int GetGLFWFromMouseCode(const MouseCode& code);
 
 private:
