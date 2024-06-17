72b97dc132c0a3acde7d71e7f24a71db94be10e5
Alexandre <beaujonalexandre@gmail.com>
2024-06-17 02:49:50 +0200

    Added support for arrow keys. Used in main file.

--- Sandbox/src/main.cpp
+++ Sandbox/src/main.cpp
@@ -6,13 +6,22 @@ int main(int argc, char** argv)
 {
 	auto app = std::make_unique<vrm::Application>(argc, argv);
 	
-	app->createTrigger("test")
+	app->createTrigger("MoveForward")
 		.bindInput(vrm::KeyCode::W)
-		.bindInput(vrm::KeyCode::A)
+		.bindInput(vrm::KeyCode::Up)
+		.bindCallback([](bool triggered) { LOG_INFO("Moving forward  : {}", triggered); });
+	app->createTrigger("MoveBackward")
 		.bindInput(vrm::KeyCode::S)
+		.bindInput(vrm::KeyCode::Down)
+		.bindCallback([](bool triggered) { LOG_INFO("Moving backward : {}", triggered); });
+	app->createTrigger("MoveLeft")
+		.bindInput(vrm::KeyCode::A)
+		.bindInput(vrm::KeyCode::Left)
+		.bindCallback([](bool triggered) { LOG_INFO("Moving left     : {}", triggered); });
+	app->createTrigger("MoveRight")
 		.bindInput(vrm::KeyCode::D)
-		.bindInput(vrm::MouseCode::Left)
-		.bindCallback([](bool triggered) { LOG_INFO("Test trigger: {}", triggered); });
+		.bindInput(vrm::KeyCode::Right)
+		.bindCallback([](bool triggered) { LOG_INFO("Moving Right    : {}", triggered); });
 
 	app->createCustomEvent("Exit")
 		.bindInput(vrm::Event::Type::KeyPressed, vrm::KeyCode::Escape)
