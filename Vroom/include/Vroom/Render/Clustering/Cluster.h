f3b9e672d7e3b0e276618ee39a16559ff7e520fc
Alexandre <beaujonalexandre@gmail.com>
2024-07-06 00:44:10 +0200

    Added algorithm: given any point P, and any cuboid C, it will find the point inside C that is the nearest from P.

--- Vroom/include/Vroom/Render/Clustering/Cluster.h
+++ Vroom/include/Vroom/Render/Clustering/Cluster.h
@@ -30,6 +30,8 @@ public:
     void setNearBottomLeft(const glm::vec3& nearBottomLeft, const glm::mat4& viewProjectionMatrix);
     void setFarTopRight(const glm::vec3& farTopRight, const glm::mat4& viewProjectionMatrix);
 
+    void setCorners(const glm::vec3& nearBottomLeft, const glm::vec3& farTopRight, const glm::mat4& viewProjectionMatrix);
+
     /**
      * @brief Checks if a point is inside the cluster where the coordinates are in NDC space.
      * 
@@ -38,6 +40,14 @@ public:
      * @return false If the point is outside the cluster.
      */
     bool containsNDC(const glm::vec3& point) const;
+
+    /**
+     * @brief Returns the closest point in the cluster to a point where the coordinates are in NDC space.
+     * 
+     * @param point Point outside the cluster.
+     * @return glm::vec3 Closest point in the cluster to the input point.
+     */
+    glm::vec3 getClosestPointNDC(const glm::vec3& point) const;
 
     /**
      * @brief Checks if a sphere intersects the cluster where the coordinates are in NDC space. This will also return true if the sphere is fully inside the cluster.
@@ -49,17 +59,43 @@ public:
      */
     bool insersectsSphereNDC(const glm::vec3& center, float radius) const;
 
+    /**
+     * @brief Checks if a point is inside the cluster where the coordinates are in world space.
+     * 
+     * @param point Point to check.
+     * @return true If the point is inside the cluster.
+     * @return false If the point is outside the cluster.
+     */
     bool containsWS(const glm::vec3& point) const;
 
+    /**
+     * @brief Returns the closest point in the cluster to a point where the coordinates are in world space.
+     * @see @ref clustered_rendering for more information about the algorithm.
+     * 
+     * @param point Point outside the cluster.
+     * @return glm::vec3 Closest point in the cluster to the input point.
+     */
+    glm::vec3 getClosestPointWS(const glm::vec3& point) const;
+
+    /**
+     * @brief Checks if a sphere intersects the cluster where the coordinates are in world space. This will also return true if the sphere is fully inside the cluster.
+     * @see @ref clustered_rendering for more information about the algorithm.
+     * 
+     * @param center Center of the sphere.
+     * @param radius Radius of the sphere.
+     * @return true If the sphere intersects the cluster.
+     * @return false If the sphere does not intersect the cluster.
+     */
     bool intersectsSphereWS(const glm::vec3& center, float radius) const;
 
 private:
     void updateWSCorners(const glm::mat4& viewProjectionMatrix);
+    
 
 private:
     glm::vec3 m_NearBottomLeft, m_FarTopRight; // In NDC space
     std::array<glm::vec3, 8> m_WSCorners; // NearBottomLeft, NearTopLeft, NearTopRight, NearBottomRight, FarBottomLeft, FarTopLeft, FarTopRight, FarBottomRight
-
+    static std::array<std::tuple<size_t, size_t, size_t>, 8> s_Neighbours;
 };
 
 } // namespace vrm
--- Vroom/src/Render/Clustering/Cluster.cpp
+++ Vroom/src/Render/Clustering/Cluster.cpp
@@ -1,10 +1,23 @@
 #include "Vroom/Render/Clustering/Cluster.h"
 
+#include <glm/gtx/norm.hpp>
+
 #include "Vroom/Core/Assert.h"
 
 namespace vrm
 {
-    
+
+std::array<std::tuple<size_t, size_t, size_t>, 8> Cluster::s_Neighbours = {
+    std::make_tuple(1, 3, 4), // 0: NearBottomLeft
+    std::make_tuple(0, 2, 5), // 1: NearTopLeft
+    std::make_tuple(1, 3, 6), // 2: NearTopRight
+    std::make_tuple(0, 2, 7), // 3: NearBottomRight
+    std::make_tuple(0, 5, 7), // 4: FarBottomLeft
+    std::make_tuple(1, 4, 6), // 5: FarTopLeft
+    std::make_tuple(2, 5, 7), // 6: FarTopRight
+    std::make_tuple(3, 4, 6)  // 7: FarBottomRight
+};
+
 Cluster::Cluster(const glm::vec3& nearBottomLeft, const glm::vec3& farTopRight, const glm::mat4& viewProjectionMatrix)
     : m_NearBottomLeft(nearBottomLeft), m_FarTopRight(farTopRight)
 {
@@ -34,6 +47,13 @@ void Cluster::setFarTopRight(const glm::vec3& farTopRight, const glm::mat4& view
     updateWSCorners(viewProjectionMatrix);
 }
 
+void Cluster::setCorners(const glm::vec3& nearBottomLeft, const glm::vec3& farTopRight, const glm::mat4& viewProjectionMatrix)
+{
+    m_NearBottomLeft = nearBottomLeft;
+    m_FarTopRight = farTopRight;
+    updateWSCorners(viewProjectionMatrix);
+}
+
 bool Cluster::containsNDC(const glm::vec3& point) const
 {
     return point.x >= m_NearBottomLeft.x && point.x <= m_FarTopRight.x &&
@@ -41,20 +61,29 @@ bool Cluster::containsNDC(const glm::vec3& point) const
            point.z >= m_NearBottomLeft.z && point.z <= m_FarTopRight.z;
 }
 
+glm::vec3 Cluster::getClosestPointNDC(const glm::vec3& P) const
+{
+    // If the point is already inside the cluster, we return it: it is the closest point.
+    if (containsNDC(P))
+        return P;
+    
+    // We search for (x, y, z), the point in the cluster that is closest to P.
+    // In NDC, the cluster is a box aligned with the axes.
+    // So we just clamp the coordinates of P to the cluster.
+    return glm::vec3(glm::clamp(P.x, m_NearBottomLeft.x, m_FarTopRight.x),
+                     glm::clamp(P.y, m_NearBottomLeft.y, m_FarTopRight.y),
+                     glm::clamp(P.z, m_NearBottomLeft.z, m_FarTopRight.z));
+}
+
 bool Cluster::insersectsSphereNDC(const glm::vec3& center, float radius) const
 {
-    // We search for (x, y, z), the point in the cluster that is closest to the center of the sphere.
-    // x is the closest point to the center of the sphere in the x axis, and so on.
-    // If the sphere center is inside the cluster, then (x, y, z) is the center of the sphere.
-    float x = glm::clamp(center.x, m_NearBottomLeft.x, m_FarTopRight.x);
-    float y = glm::clamp(center.y, m_NearBottomLeft.y, m_FarTopRight.y);
-    float z = glm::clamp(center.z, m_NearBottomLeft.z, m_FarTopRight.z);
+    // Getting the closest point in the cluster to the sphere center.
+    auto C = getClosestPointNDC(center);
 
-    // Now we calculate the squared distance between the sphere center and (x, y, z).
-    // If the sphere center is inside the cluster, the distance will be 0.
-    float sqrDistance = (x - center.x) * (x - center.x) +
-                        (y - center.y) * (y - center.y) +
-                        (z - center.z) * (z - center.z);
+    // Now we calculate the squared distance between the sphere center and C.
+    float sqrDistance = (C.x - center.x) * (C.x - center.x) +
+                        (C.y - center.y) * (C.y - center.y) +
+                        (C.z - center.z) * (C.z - center.z);
 
     // If the sphere center is outside the cluster, the radius has to be big enough to intersect the cluster.
     // If the sphere center is inside the cluster, then the distance is 0. This means it will return true for any positive radius.
@@ -63,14 +92,103 @@ bool Cluster::insersectsSphereNDC(const glm::vec3& center, float radius) const
 
 bool Cluster::containsWS(const glm::vec3& point) const
 {
-    return point.x >= m_WSCorners[0].x && point.x <= m_WSCorners[6].x &&
-           point.y >= m_WSCorners[0].y && point.y <= m_WSCorners[6].y &&
-           point.z >= m_WSCorners[0].z && point.z <= m_WSCorners[6].z;
+    VRM_ASSERT_MSG(false, "Not implemented yet.");
+}
+
+glm::vec3 Cluster::getClosestPointWS(const glm::vec3& P) const
+{
+    if (containsWS(P))
+        return P;
+    
+    // Searching for D, the point in the cluster that is closest to P.
+    // iD is the index of D.
+    size_t iD = 0;
+    float minDistance = glm::length2(m_WSCorners[iD] - P);
+
+    for (size_t i = 1; i < m_WSCorners.size(); ++i)
+    {
+        float distance = glm::length2(m_WSCorners[i] - P);
+        if (distance < minDistance)
+        {
+            minDistance = distance;
+            iD = i;
+        }
+    }
+    
+    const glm::vec3& D = m_WSCorners[iD];
+
+    // Searching for A, B, C, the three neighbours of D.
+    auto [iA, iB, iC] = s_Neighbours[iD];
+    const glm::vec3& A = m_WSCorners[iA];
+    const glm::vec3& B = m_WSCorners[iB];
+    const glm::vec3& C = m_WSCorners[iC];
+
+    // If DP.DG < 0 for G in {A, B, C}, then D is the closest point.
+    const glm::vec3 DP = P - D;
+    const glm::vec3 DA = A - D;
+    const glm::vec3 DB = B - D;
+    const glm::vec3 DC = C - D;
+
+    const float DPDA = glm::dot(DP, DA);
+    const float DPDB = glm::dot(DP, DB);
+    const float DPDC = glm::dot(DP, DC);
+
+    if (DPDA < 0.f && DPDB < 0.f && DPDC < 0.f)
+        return D;
+    
+    // Finding M, M' in {A, B, C} such that DP.DM / (||DP|| * ||DM||) and DP.DM' / (||DP|| * ||DM'||) are maximized.
+    /// @todo I think we can optimize this with squared cosines.
+    const float cosDPDA = DPDA / (glm::length(DP) * glm::length(DA));
+    const float cosDPDB = DPDB / (glm::length(DP) * glm::length(DB));
+    const float cosDPDC = DPDC / (glm::length(DP) * glm::length(DC));
+
+    glm::vec3 DM, DMp;
+
+    if (cosDPDA < cosDPDB && cosDPDA < cosDPDC)
+    {
+        DM = DB;
+        DMp = DC;
+    }
+    else if (cosDPDB < cosDPDA && cosDPDB < cosDPDC)
+    {
+        DM = DA;
+        DMp = DC;
+    }
+    else
+    {
+        DM = DA;
+        DMp = DB;
+    }
+
+    const glm::vec3 nDM = glm::normalize(DM);
+    const glm::vec3 nDMp = glm::normalize(DMp);
+
+    // PI1 is the orthogonal projection of DP on DM.
+    // PI2 is the orthogonal projection of DP on DM'.
+    glm::vec3 PI1 = glm::dot(DP, nDM) * nDM;
+    glm::vec3 PI2 = glm::dot(DP, nDMp) * nDMp;
+
+    // PI0 is the orthogonal projection of P on the plane defined by D, M, M'.
+    glm::vec3 PI0 = D + PI1 + PI2;
+
+    return PI0;
 }
 
 bool Cluster::intersectsSphereWS(const glm::vec3& center, float radius) const
 {
-    VRM_ASSERT_MSG(false, "Not implemented yet.");
+    // Getting the closest point in the cluster to the sphere center.
+    auto C = getClosestPointWS(center);
+    if (C == center)
+        return true;
+
+    // Now we calculate the squared distance between the sphere center and C.
+    float sqrDistance = (C.x - center.x) * (C.x - center.x) +
+                        (C.y - center.y) * (C.y - center.y) +
+                        (C.z - center.z) * (C.z - center.z);
+
+    // If the sphere center is outside the cluster, the radius has to be big enough to intersect the cluster.
+    // If the sphere center is inside the cluster, then the distance is 0. This means it will return true for any positive radius.
+    return sqrDistance < radius * radius;
 }
 
 void Cluster::updateWSCorners(const glm::mat4& viewProjectionMatrix)
