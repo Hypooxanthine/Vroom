739872f0a9864cc0a3fdac8f08881cc40542bfc2
Alexandre <beaujonalexandre@gmail.com>
2024-07-05 17:09:21 +0200

    Restructured cluster so that it can find intersections in world space too, not only NDC.

--- Vroom/include/Vroom/Render/Clustering/Cluster.h
+++ Vroom/include/Vroom/Render/Clustering/Cluster.h
@@ -1,12 +1,13 @@
 #pragma once
 
+#include <array>
 #include <glm/glm.hpp>
 
 namespace vrm
 {
 
 /**
- * @brief Represents a cluster in the clustering algorithm. Coordinates are in the NDC space.
+ * @brief Represents a cluster in the clustering algorithm.
  * For each fragment, it will be required to convert its position to NDC space to check if it is inside a cluster.
  * 
  */
@@ -21,36 +22,43 @@ public:
     Cluster& operator=(const Cluster&) = default;
     Cluster& operator=(Cluster&&) = default;
     
-    Cluster(const glm::vec3& nearBottomLeft, const glm::vec3& farTopRight);
+    Cluster(const glm::vec3& nearBottomLeft, const glm::vec3& farTopRight, const glm::mat4& viewProjectionMatrix);
 
     const glm::vec3& getNearBottomLeft() const;
     const glm::vec3& getFarTopRight() const;
 
-    void setNearBottomLeft(const glm::vec3& nearBottomLeft);
-    void setFarTopRight(const glm::vec3& farTopRight);
+    void setNearBottomLeft(const glm::vec3& nearBottomLeft, const glm::mat4& viewProjectionMatrix);
+    void setFarTopRight(const glm::vec3& farTopRight, const glm::mat4& viewProjectionMatrix);
 
     /**
-     * @brief Checks if a point is inside the cluster.
+     * @brief Checks if a point is inside the cluster where the coordinates are in NDC space.
      * 
      * @param point Point to check.
      * @return true If the point is inside the cluster.
      * @return false If the point is outside the cluster.
      */
-    bool contains(const glm::vec3& point) const;
+    bool containsNDC(const glm::vec3& point) const;
 
     /**
-     * @brief Checks if a sphere intersects the cluster. This will also return true if the sphere is fully inside the cluster.
+     * @brief Checks if a sphere intersects the cluster where the coordinates are in NDC space. This will also return true if the sphere is fully inside the cluster.
      * 
      * @param center Center of the sphere.
      * @param radius Radius of the sphere.
      * @return true If the sphere intersects the cluster.
      * @return false If the sphere does not intersect the cluster.
      */
-    bool insersectsSphere(const glm::vec3& center, float radius) const;
+    bool insersectsSphereNDC(const glm::vec3& center, float radius) const;
+
+    bool containsWS(const glm::vec3& point) const;
+
+    bool intersectsSphereWS(const glm::vec3& center, float radius) const;
 
 private:
-    glm::vec3 m_NearBottomLeft;
-    glm::vec3 m_FarTopRight;
+    void updateWSCorners(const glm::mat4& viewProjectionMatrix);
+
+private:
+    glm::vec3 m_NearBottomLeft, m_FarTopRight; // In NDC space
+    std::array<glm::vec3, 8> m_WSCorners; // NearBottomLeft, NearTopLeft, NearTopRight, NearBottomRight, FarBottomLeft, FarTopLeft, FarTopRight, FarBottomRight
 
 };
 
--- Vroom/src/Render/Clustering/Cluster.cpp
+++ Vroom/src/Render/Clustering/Cluster.cpp
@@ -1,11 +1,14 @@
 #include "Vroom/Render/Clustering/Cluster.h"
 
+#include "Vroom/Core/Assert.h"
+
 namespace vrm
 {
     
-Cluster::Cluster(const glm::vec3& nearBottomLeft, const glm::vec3& farTopRight)
+Cluster::Cluster(const glm::vec3& nearBottomLeft, const glm::vec3& farTopRight, const glm::mat4& viewProjectionMatrix)
     : m_NearBottomLeft(nearBottomLeft), m_FarTopRight(farTopRight)
 {
+    updateWSCorners(viewProjectionMatrix);
 }
 
 const glm::vec3& Cluster::getNearBottomLeft() const
@@ -18,24 +21,27 @@ const glm::vec3& Cluster::getFarTopRight() const
     return m_FarTopRight;
 }
 
-void Cluster::setNearBottomLeft(const glm::vec3& nearBottomLeft)
+void Cluster::setNearBottomLeft(const glm::vec3& nearBottomLeft, const glm::mat4& viewProjectionMatrix)
 {
     m_NearBottomLeft = nearBottomLeft;
+    updateWSCorners(viewProjectionMatrix);
+
 }
 
-void Cluster::setFarTopRight(const glm::vec3& farTopRight)
+void Cluster::setFarTopRight(const glm::vec3& farTopRight, const glm::mat4& viewProjectionMatrix)
 {
     m_FarTopRight = farTopRight;
+    updateWSCorners(viewProjectionMatrix);
 }
 
-bool Cluster::contains(const glm::vec3& point) const
+bool Cluster::containsNDC(const glm::vec3& point) const
 {
     return point.x >= m_NearBottomLeft.x && point.x <= m_FarTopRight.x &&
            point.y >= m_NearBottomLeft.y && point.y <= m_FarTopRight.y &&
            point.z >= m_NearBottomLeft.z && point.z <= m_FarTopRight.z;
 }
 
-bool Cluster::insersectsSphere(const glm::vec3& center, float radius) const
+bool Cluster::insersectsSphereNDC(const glm::vec3& center, float radius) const
 {
     // We search for (x, y, z), the point in the cluster that is closest to the center of the sphere.
     // x is the closest point to the center of the sphere in the x axis, and so on.
@@ -53,6 +59,44 @@ bool Cluster::insersectsSphere(const glm::vec3& center, float radius) const
     // If the sphere center is outside the cluster, the radius has to be big enough to intersect the cluster.
     // If the sphere center is inside the cluster, then the distance is 0. This means it will return true for any positive radius.
     return sqrDistance < radius * radius;
+}
+
+bool Cluster::containsWS(const glm::vec3& point) const
+{
+    return point.x >= m_WSCorners[0].x && point.x <= m_WSCorners[6].x &&
+           point.y >= m_WSCorners[0].y && point.y <= m_WSCorners[6].y &&
+           point.z >= m_WSCorners[0].z && point.z <= m_WSCorners[6].z;
+}
+
+bool Cluster::intersectsSphereWS(const glm::vec3& center, float radius) const
+{
+    VRM_ASSERT_MSG(false, "Not implemented yet.");
+}
+
+void Cluster::updateWSCorners(const glm::mat4& viewProjectionMatrix)
+{
+    // NearBottomLeft
+    m_WSCorners[0] = glm::vec3(m_NearBottomLeft.x, m_NearBottomLeft.y, m_NearBottomLeft.z);
+    // NearTopLeft
+    m_WSCorners[1] = glm::vec3(m_NearBottomLeft.x, m_FarTopRight.y, m_NearBottomLeft.z);
+    // NearTopRight
+    m_WSCorners[2] = glm::vec3(m_FarTopRight.x, m_FarTopRight.y, m_NearBottomLeft.z);
+    // NearBottomRight
+    m_WSCorners[3] = glm::vec3(m_FarTopRight.x, m_NearBottomLeft.y, m_NearBottomLeft.z);
+    // FarBottomLeft
+    m_WSCorners[4] = glm::vec3(m_NearBottomLeft.x, m_NearBottomLeft.y, m_FarTopRight.z);
+    // FarTopLeft
+    m_WSCorners[5] = glm::vec3(m_NearBottomLeft.x, m_FarTopRight.y, m_FarTopRight.z);
+    // FarTopRight
+    m_WSCorners[6] = glm::vec3(m_FarTopRight.x, m_FarTopRight.y, m_FarTopRight.z);
+    // FarBottomRight
+    m_WSCorners[7] = glm::vec3(m_FarTopRight.x, m_NearBottomLeft.y, m_FarTopRight.z);
+
+    for (auto& corner : m_WSCorners)
+    {
+        glm::vec4 corner4 = viewProjectionMatrix * glm::vec4(corner, 1.f);
+        corner = glm::vec3(corner4) / corner4.w;
+    }
 }
 
 } // namespace vrm
