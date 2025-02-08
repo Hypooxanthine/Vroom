e5e1784c0efed7eb87ad6d66c2295e9f93599e3b
Alexandre Beaujon <beaujonalexandre@gmail.com>
2025-02-08 17:03:45 +0100

    Basic abstraction

---
+++ Vroom/include/Vroom/Render/Abstraction/BufferGL.h
@@ -0,0 +1,183 @@
+#pragma once
+
+#include "Vroom/Render/Abstraction/GLCall.h"
+
+namespace vrm
+{
+
+template <GLenum TARGET>
+class BufferGL
+{
+public:
+    inline constexpr Buffer()
+    {
+
+    }
+
+    inline explicit Buffer(const Buffer& other)
+    {
+        *this = other;
+    }
+
+    inline explicit Buffer(Buffer&& other) noexcept
+    {
+        *this = std::move(other);
+    }
+
+    inline virtual ~Buffer() noexcept
+    {
+        release();
+    }
+
+    inline Buffer& operator=(const Buffer& other)
+    {
+        release();
+
+        if (other.isValid())
+        {
+            glBindBuffer(GL_COPY_READ_BUFFER, other.getRenderId());
+
+            GLint size;
+            glGetBufferParameteriv(GL_COPY_READ_BUFFER, GL_BUFFER_SIZE, &size);
+            GLint usage;
+            glGetBufferParameteriv(GL_COPY_READ_BUFFER, GL_BUFFER_USAGE, &usage);
+
+            generate(nullptr, (GLenum)size, (GLint)usage);
+            glBindBuffer(GL_COPY_WRITE_BUFFER, getRenderId());
+
+            glCopyBufferSubData(GL_COPY_READ_BUFFER, GL_COPY_WRITE_BUFFER, 0, 0, size);
+        }
+
+        return *this;
+    }
+
+    inline Buffer& operator=(Buffer&& other) noexcept
+    {
+        release();
+
+        if (other.isValid())
+        {
+            m_RenderID = other.m_RenderID;
+            other.m_RenderID = 0;
+        }
+
+        return *this;
+    }
+
+    /**
+     * @brief Create a buffer on gpu.
+     */
+    inline GLuint generate()
+    {
+        release();
+        glGenBuffers(1, &m_RenderID);
+
+        return m_RenderID;
+    }
+
+    /**
+     * @brief (Re)assign buffer data.
+     * 
+     * @param data The data
+     * @param size Data size (byte count)
+     * @param usage GL_STREAM_DRAW, GL_STATIC_DRAW, or GL_DYNAMIC_DRAW
+     */
+    inline void setData(const void* data, GLsizeiptr size, GLenum usage)
+    {
+        bind();
+        glBufferData(TARGET, size, data, usage);
+    }
+
+    /**
+     * @brief Set buffer sub data. Be careful with buffer size you set.
+     * 
+     * @param data The data
+     * @param size Data size (byte count)
+     * @param offset Where the data should be in the buffer (in bytes)
+     */
+    inline void setSubData(const void* data, GLsizeiptr size, GLintptr offset)
+    {
+        bind();
+        glBufferSubData(TARGET, offset, size, data);
+    }
+
+    /**
+     * @brief Create a buffer and set its data
+     * 
+     * @param data The data
+     * @param size Data size (byte count)
+     * @param usage GL_STREAM_DRAW, GL_STATIC_DRAW, or GL_DYNAMIC_DRAW
+     * @return GLuint The render id of the created buffer
+     */
+    inline GLuint generate(const void* data, GLsizeiptr size, GLenum usage)
+    {
+        generate();
+        setData(data, size, usage);
+        return m_RenderId;
+    }
+
+    /**
+     * @brief Deletes the buffer. Does nothing if the buffer was not generated.
+     * 
+     */
+    inline void release()
+    {
+        glDeleteBuffers(1, &m_RenderID);
+        m_RenderID = 0;
+    }
+
+    /**
+     * @brief Binds a buffer of target TARGET.
+     * 
+     * @param renderId Render id of the buffer
+     */
+    inline static void Bind(GLuint renderId)
+    {
+        glBindBuffer(TARGET, renderId);
+    }
+
+    /**
+     * @brief Unbinds the currently bound buffer of target TARGET.
+     * 
+     */
+    inline static void Unbind()
+    {
+        Bind(0);
+    }
+
+    /**
+     * @brief Binds this buffer.
+     * 
+     */
+    inline void bind() const
+    {
+        BufferGL<TARGET>::Bind(m_RenderID);
+    }
+
+    /**
+     * @brief Unbinds this buffer (unbinds any buffer of target TARGET).
+     * 
+     */
+    inline void unbind() const
+    {
+        Unbind();
+    }
+
+    /**
+     * @brief Get the Render Id object
+     * 
+     * @return constexpr GLuint The render id
+     */
+    inline constexpr GLuint getRenderId() const { return m_RenderID; }
+
+    /** 
+     * @return true if the buffer is valid (was created)
+     * @return false if the buffer is not valid (was not created or has been deleted)
+     */
+    inline constexpr bool isValid() const { return getRenderId() != 0; }
+
+private:
+    GLuint m_RenderID = 0;
+};
+
+} // namespace vrm
