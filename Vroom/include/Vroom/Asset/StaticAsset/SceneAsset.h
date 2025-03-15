f4ad40ef2df0b9bb7af29e0da83d8e27d3287417
Alexandre <beaujonalexandre@gmail.com>
2025-03-16 00:34:30 +0100

    Scene asset data is a nlohmann::json

--- Vroom/include/Vroom/Asset/StaticAsset/SceneAsset.h
+++ Vroom/include/Vroom/Asset/StaticAsset/SceneAsset.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <nlohmann/json.hpp>
+
 #include "Vroom/Asset/StaticAsset/StaticAsset.h"
 #include "Vroom/Asset/AssetInstance/SceneInstance.h"
 
@@ -17,10 +19,13 @@ public:
 
   [[nodiscard]] SceneInstance createInstance();
 
+  const nlohmann::json& getSceneData() const { return m_SceneData; }
+
 protected:
   bool loadImpl(const std::string &filePath) override;
 
 private:
+  nlohmann::json m_SceneData;
   
 };
 
--- Vroom/src/Asset/StaticAsset/SceneAsset.cpp
+++ Vroom/src/Asset/StaticAsset/SceneAsset.cpp
@@ -1,8 +1,9 @@
 #include "Vroom/Asset/StaticAsset/SceneAsset.h"
 
 #include <nlohmann/json.hpp>
+#include <fstream>
 
-#include "Vroom/Core/Log.h"
+#include "Vroom/Core/Assert.h"
 
 namespace vrm
 {
@@ -24,7 +25,15 @@ bool SceneAsset::loadImpl(const std::string &filePath)
 {
   using json = nlohmann::json;
 
-  return false;
+  std::ifstream ifs;
+  ifs.open(filePath);
+  if (!ifs.is_open())
+    return false;
+
+  m_SceneData.clear();
+  ifs >> m_SceneData;
+
+  return true;
 }
 
 } // namespace vrm
