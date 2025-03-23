26335c3009266462cc44af3f88d2bdaf297b30d0
Alexandre Beaujon <beaujonalexandre@gmail.com>
2025-03-23 18:22:43 +0100

    fps/frame time in stats panel are now mean values in sample time

--- VroomEditor/src/UserInterface/StatisticsPanel.cpp
+++ VroomEditor/src/UserInterface/StatisticsPanel.cpp
@@ -18,12 +18,28 @@ bool StatisticsPanel::onImgui()
 {
   bool ret = false;
   float dt = Application::Get().getDeltaTime();
-  float fps = 1.f / dt;
+  static float meanDt = dt;
+  static size_t framesCounter = 0;
+  static float meanFps = 1.f / dt;
+  
+  static float timeAccumulator = 0.f;
+  constexpr float sampleTime = 2.f;
+
+  timeAccumulator += dt;
+  ++framesCounter;
+
+  if (timeAccumulator > sampleTime)
+  {
+    meanFps = static_cast<float>(framesCounter) / sampleTime;
+    meanDt = 1.f / meanFps;
+    timeAccumulator -= sampleTime;
+    framesCounter = 0;
+  }
 
   if (ImGui::Begin("Statistics"))
   {
-    ImGui::Text("Frame time: %.5f s", dt);
-    ImGui::Text("Frame rate: %.0f FPS", fps);
+    ImGui::Text("Frame time: %.5f s", meanDt);
+    ImGui::Text("Frame rate: %.0f FPS", meanFps);
   }
   ImGui::End();
 
