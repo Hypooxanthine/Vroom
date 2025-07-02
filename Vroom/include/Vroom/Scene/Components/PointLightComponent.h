2d7e45463d120763e72a4ef5a7c54f34e82fd179
Alexandre <beaujonalexandre@gmail.com>
2025-07-02 21:57:34 +0200

    Adjusted default light component values

--- Vroom/include/Vroom/Scene/Components/PointLightComponent.h
+++ Vroom/include/Vroom/Scene/Components/PointLightComponent.h
@@ -9,11 +9,11 @@ namespace vrm
   struct VRM_API PointLightComponent
   {
     glm::vec3 color = glm::vec3(1.0f);
-    float intensity = 100.0f;
-    float radius = 50.0f;
+    float intensity = 5.0f;
+    float radius = 30.0f;
     float constantAttenuation = 1.0f;
-    float linearAttenuation = 0.09f;
-    float quadraticAttenuation = 0.032f;
+    float linearAttenuation = 0.0f;
+    float quadraticAttenuation = 0.1f;
   };
 
 } // namespace vrm
