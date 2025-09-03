e519f9a442e5281bf082369d965729b6ae70312d
Alexandre <alexandre@mail.com>
2025-09-03 22:50:57 +0200

    Got back to cpp20 because no use of cpp23 + problems with clangd on windows with cp23

--- Vroom/include/Vroom/Render/Abstraction/Texture.h
+++ Vroom/include/Vroom/Render/Abstraction/Texture.h
@@ -1,179 +1,184 @@
 #pragma once
 
-#include <cmath>
 #include <array>
+#include <cmath>
 
+#include "Vroom/Asset/AssetData/TextureData.h"
 #include "Vroom/Core/Assert.h"
 #include "Vroom/Render/Abstraction/GLCall.h"
-#include "Vroom/Asset/AssetData/TextureData.h"
+
 
 namespace vrm::gl
 {
-  
-  class Texture
+
+class Texture
+{
+public:
+
+  struct Desc
   {
-  public:
-
-    struct Desc
-    {
-      GLuint dimension = 0;
-      GLsizei width = 0, height = 0, depth = 0;
-      GLint mipmapCount = 1;
-      GLint internalFormat = 0;
-      GLenum format = 0;
-      GLuint sampleCount = 1;
-      GLboolean fixedSampleLocations = GL_TRUE;
-      bool layered = false;
-      bool cubemap = false;
-    };
-  
-  public:
-
-    Texture();
-    ~Texture();
-
-    Texture& operator=(const Texture&) = delete;
-    Texture(const Texture&) = delete;
-
-    Texture& operator=(Texture&&) = delete;
-    Texture(Texture&&) = delete;
-
-    inline static constexpr GLenum ToGlFormat(TextureFormat format);
-    inline static constexpr GLint ToGlInternalFormat(TextureFormat format);
-    inline static constexpr GLenum GetBindingTarget(GLuint layers, GLuint samples);
-
-
-    inline static GLint GetMaxMipMapCount(GLsizei width, GLsizei height);
-    inline static constexpr GLint GetBasicColorInternalFormat(GLuint channelCount, GLuint bitsPerChannel);
-    inline static constexpr GLenum GetBasicColorFormat(GLuint channelCount);
-
-    inline static constexpr GLuint GetChannelCountFromFormat(GLenum format);
-
-    void create(const Desc& desc);
-    void release();
-    void bind(GLenum target) const;
-    void bind() const;
-
-    inline bool isCreated() const { return m_renderId != 0; }
-    inline GLuint getRenderId() const { return m_renderId; }
-    inline const Desc& getDescription() const { return m_desc; }
-    inline GLenum getDefaultTarget() const { return m_defaultTarget; }
-
-  private:
-
-    GLuint m_renderId = 0;
-    GLenum m_defaultTarget = 0;
-    Desc m_desc = {};
-
+    GLuint    dimension = 0;
+    GLsizei   width = 0, height = 0, depth = 0;
+    GLint     mipmapCount          = 1;
+    GLint     internalFormat       = 0;
+    GLenum    format               = 0;
+    GLuint    sampleCount          = 1;
+    GLboolean fixedSampleLocations = GL_TRUE;
+    bool      layered              = false;
+    bool      cubemap              = false;
   };
 
-  inline constexpr GLenum Texture::ToGlFormat(TextureFormat format)
-  {
-    switch (format)
-    {
-    case TextureFormat::Grayscale:
-      return GL_RED;
-    case TextureFormat::RG:
-      return GL_RG;
-    case TextureFormat::RGB:
-      return GL_RGB;
-    case TextureFormat::RGBA:
-      return GL_RGBA;
-    default:
-      VRM_ASSERT_MSG(false, "Unsupported format.");
-      return 0;
-    }
-  }
+public:
 
-  inline constexpr GLint Texture::ToGlInternalFormat(TextureFormat format)
-  {
-    switch (format)
-    {
-    case TextureFormat::Grayscale:
-      return GL_R8;
-    case TextureFormat::RG:
-      return GL_RG8;
-    case TextureFormat::RGB:
-      return GL_RGB8;
-    case TextureFormat::RGBA:
-      return GL_RGBA8;
-    default:
-      VRM_ASSERT_MSG(false, "Unsupported format.");
-      return 0;
-    }
-  }
+  Texture();
+  ~Texture();
 
-  inline constexpr GLenum Texture::GetBindingTarget(GLuint layers, GLuint samples)
-  {
-    if (layers > 1)
-    {
-      return samples > 1 ? GL_TEXTURE_2D_MULTISAMPLE_ARRAY : GL_TEXTURE_2D_ARRAY;
-    }
-    else
-    {
-      return samples > 1 ? GL_TEXTURE_2D_MULTISAMPLE : GL_TEXTURE_2D;
-    }
-  }
+  Texture& operator=(const Texture&) = delete;
+  Texture(const Texture&)            = delete;
 
-  inline GLint Texture::GetMaxMipMapCount(GLsizei width, GLsizei height)
-  {
-    return 1 + static_cast<GLint>(std::floor(std::log2(std::max(width, height))));
-  }
+  Texture& operator=(Texture&&) = delete;
+  Texture(Texture&&)            = delete;
 
-  inline constexpr GLint Texture::GetBasicColorInternalFormat(GLuint channelCount, GLuint bitsPerChannel)
+  inline static constexpr GLenum ToGlFormat(TextureFormat format);
+  inline static constexpr GLint  ToGlInternalFormat(TextureFormat format);
+  inline static constexpr GLenum GetBindingTarget(GLuint layers,
+                                                  GLuint samples);
+
+  inline static GLint  GetMaxMipMapCount(GLsizei width, GLsizei height);
+  inline static GLint  GetBasicColorInternalFormat(GLuint channelCount,
+                                                   GLuint bitsPerChannel);
+  inline static GLenum GetBasicColorFormat(GLuint channelCount);
+
+  inline static constexpr GLuint GetChannelCountFromFormat(GLenum format);
+
+  void create(const Desc& desc);
+  void release();
+  void bind(GLenum target) const;
+  void bind() const;
+
+  inline bool        isCreated() const { return m_renderId != 0; }
+  inline GLuint      getRenderId() const { return m_renderId; }
+  inline const Desc& getDescription() const { return m_desc; }
+  inline GLenum      getDefaultTarget() const { return m_defaultTarget; }
+
+private:
+
+  GLuint m_renderId      = 0;
+  GLenum m_defaultTarget = 0;
+  Desc   m_desc          = {};
+};
+
+inline constexpr GLenum Texture::ToGlFormat(TextureFormat format)
+{
+  switch (format)
   {
-    static constexpr std::array<std::array<GLenum, 3>, 4> s_assoc = {{
-      // 1 channel (RED)
+  case TextureFormat::Grayscale:
+    return GL_RED;
+  case TextureFormat::RG:
+    return GL_RG;
+  case TextureFormat::RGB:
+    return GL_RGB;
+  case TextureFormat::RGBA:
+    return GL_RGBA;
+  default:
+    VRM_ASSERT_MSG(false, "Unsupported format.");
+    return 0;
+  }
+}
+
+inline constexpr GLint Texture::ToGlInternalFormat(TextureFormat format)
+{
+  switch (format)
+  {
+  case TextureFormat::Grayscale:
+    return GL_R8;
+  case TextureFormat::RG:
+    return GL_RG8;
+  case TextureFormat::RGB:
+    return GL_RGB8;
+  case TextureFormat::RGBA:
+    return GL_RGBA8;
+  default:
+    VRM_ASSERT_MSG(false, "Unsupported format.");
+    return 0;
+  }
+}
+
+inline constexpr GLenum Texture::GetBindingTarget(GLuint layers, GLuint samples)
+{
+  if (layers > 1)
+  {
+    return samples > 1 ? GL_TEXTURE_2D_MULTISAMPLE_ARRAY : GL_TEXTURE_2D_ARRAY;
+  }
+  else { return samples > 1 ? GL_TEXTURE_2D_MULTISAMPLE : GL_TEXTURE_2D; }
+}
+
+inline GLint Texture::GetMaxMipMapCount(GLsizei width, GLsizei height)
+{
+  return 1 + static_cast<GLint>(std::floor(std::log2(std::max(width, height))));
+}
+
+inline GLint Texture::GetBasicColorInternalFormat(GLuint channelCount,
+                                                  GLuint bitsPerChannel)
+{
+  static constexpr std::array<std::array<GLenum, 3>, 4> s_assoc = {
+    { // 1 channel (RED)
       { GL_R8, GL_R16, GL_R32F },
-      // 2 channels (RG)
+     // 2 channels (RG)
       { GL_RG8, GL_RG16, GL_RG32F },
-      // 3 channels (RGB)
+     // 3 channels (RGB)
       { GL_RGB8, GL_RGB16, GL_RGB32F },
-      // 4 channels (RGBA)
-      { GL_RGBA8, GL_RGBA16, GL_RGBA32F }
-    }};
+     // 4 channels (RGBA)
+      { GL_RGBA8, GL_RGBA16, GL_RGBA32F } }
+  };
 
-    size_t bitsIndex = 0;
-    switch (bitsPerChannel)
-    {
-      case 8:  bitsIndex = 0; break;
-      case 16: bitsIndex = 1; break;
-      case 32: bitsIndex = 2; break;
-      default:
-        throw std::out_of_range("bitsPerChannel must be 8, 16 or 32");
-    }
-
-    if (channelCount < 1 || channelCount > 4)
-      throw std::out_of_range("channelCount must be in [1, 4]");
-
-    return s_assoc[channelCount - 1][bitsIndex];
-  }
-
-  inline constexpr GLenum Texture::GetBasicColorFormat(GLuint channelCount)
+  size_t bitsIndex = 0;
+  switch (bitsPerChannel)
   {
-    static std::array<GLenum, 4> s_assoc = { GL_RED, GL_RG, GL_RGB, GL_RGBA };
-
-    if (channelCount < 1 || channelCount > 4)
-      throw std::out_of_range("channelCount must be in [1, 4]");
-
-    return s_assoc[channelCount - 1];
+  case 8:
+    bitsIndex = 0;
+    break;
+  case 16:
+    bitsIndex = 1;
+    break;
+  case 32:
+    bitsIndex = 2;
+    break;
+  default:
+    throw std::out_of_range("bitsPerChannel must be 8, 16 or 32");
   }
 
-  inline constexpr GLuint Texture::GetChannelCountFromFormat(GLenum format)
+  if (channelCount < 1 || channelCount > 4)
+    throw std::out_of_range("channelCount must be in [1, 4]");
+
+  return s_assoc[channelCount - 1][bitsIndex];
+}
+
+inline GLenum Texture::GetBasicColorFormat(GLuint channelCount)
+{
+  static std::array<GLenum, 4> s_assoc = { GL_RED, GL_RG, GL_RGB, GL_RGBA };
+
+  if (channelCount < 1 || channelCount > 4)
+    throw std::out_of_range("channelCount must be in [1, 4]");
+
+  return s_assoc[channelCount - 1];
+}
+
+inline constexpr GLuint Texture::GetChannelCountFromFormat(GLenum format)
+{
+  switch (format)
   {
-    switch (format)
-    {
-      case GL_RED:
-        return 1;
-      case GL_RG:
-        return 2;
-      case GL_RGB:
-        return 3;
-      case GL_RGBA:
-        return 4;
-      default:
-        return -1;
-    }
+  case GL_RED:
+    return 1;
+  case GL_RG:
+    return 2;
+  case GL_RGB:
+    return 3;
+  case GL_RGBA:
+    return 4;
+  default:
+    return -1;
   }
+}
 
 } // namespace vrm::gl
--- VroomEditor/include/VroomEditor/UserInterface/ParticleSystem/EmitterScalarEditor.h
+++ VroomEditor/include/VroomEditor/UserInterface/ParticleSystem/EmitterScalarEditor.h
@@ -1,62 +1,79 @@
 #pragma once
 
-#include "VroomEditor/UserInterface/ImGuiElement.h"
+#include <array>
 #include <cstddef>
+#include <span>
+#include <vector>
+
+#include "VroomEditor/UserInterface/ImGuiElement.h"
 
 namespace vrm::editor
 {
 
-  class EmitterScalarEditor : public ImGuiElement
+class EmitterScalarEditor : public ImGuiElement
+{
+public:
+
+  enum class EScalarType
   {
-  public:
-
-    enum class EScalarType
-    {
-      eScalar = 0, eVec2, eVec3, eVec4,
-      eColor3, eColor4,
-      eCount
-    };
-
-    struct Settings
-    {
-      EScalarType scalarType = EScalarType::eScalar;
-      std::vector<float> minValue, maxValue, defaultValue;
-      bool scaleLocked = false;
-    };
-
-  public:
-
-    inline bool getModified() const { return m_dataModified; }
-
-    void setSettings(const Settings& settings);
-    void setScalarType(EScalarType type);
-    void setBounds(std::span<float const> min, std::span<float const> max);
-    void setLockScale(bool lock);
-    void setData(std::span<float const> data);
-
-    inline EScalarType getScalarType() const { return m_type; }
-    inline size_t getDimension() const { return m_dim; }
-    inline bool isScaleLocked() const { return m_scaleLock; }
-    inline std::span<float const> getMin() const { return std::span(m_min.data(), m_dim); }
-    inline std::span<float const> getMax() const { return std::span(m_max.data(), m_dim); }
-    inline bool isColorType() const { return m_type == EScalarType::eColor3 || m_type == EScalarType::eColor4; }
-    std::span<float const> getData() const;
-
-  protected:
-
-    void onImgui() override;
-
-    inline void markDataModified() { m_dataModified = true; }
-
-  private:
-
-    EScalarType m_type = EScalarType::eScalar;
-    bool m_dataModified = true;
-    size_t m_dim = 1;
-    bool m_scaleLock = false;
-    std::array<float, 4> m_min = { 0.f, 0.f, 0.f, 0.f};
-    std::array<float, 4> m_max = { 1.f, 1.f, 1.f, 1.f};
-    std::array<float, 4> m_data = { 0.f, 0.f, 0.f, 0.f };
+    eScalar = 0,
+    eVec2,
+    eVec3,
+    eVec4,
+    eColor3,
+    eColor4,
+    eCount
   };
 
-}
+  struct Settings
+  {
+    EScalarType        scalarType = EScalarType::eScalar;
+    std::vector<float> minValue, maxValue, defaultValue;
+    bool               scaleLocked = false;
+  };
+
+public:
+
+  inline bool getModified() const { return m_dataModified; }
+
+  void setSettings(const Settings& settings);
+  void setScalarType(EScalarType type);
+  void setBounds(std::span<float const> min, std::span<float const> max);
+  void setLockScale(bool lock);
+  void setData(std::span<float const> data);
+
+  inline EScalarType            getScalarType() const { return m_type; }
+  inline size_t                 getDimension() const { return m_dim; }
+  inline bool                   isScaleLocked() const { return m_scaleLock; }
+  inline std::span<float const> getMin() const
+  {
+    return std::span(m_min.data(), m_dim);
+  }
+  inline std::span<float const> getMax() const
+  {
+    return std::span(m_max.data(), m_dim);
+  }
+  inline bool isColorType() const
+  {
+    return m_type == EScalarType::eColor3 || m_type == EScalarType::eColor4;
+  }
+  std::span<float const> getData() const;
+
+protected:
+
+  void onImgui() override;
+
+  inline void markDataModified() { m_dataModified = true; }
+
+private:
+
+  EScalarType          m_type         = EScalarType::eScalar;
+  bool                 m_dataModified = true;
+  size_t               m_dim          = 1;
+  bool                 m_scaleLock    = false;
+  std::array<float, 4> m_min          = { 0.f, 0.f, 0.f, 0.f };
+  std::array<float, 4> m_max          = { 1.f, 1.f, 1.f, 1.f };
+  std::array<float, 4> m_data         = { 0.f, 0.f, 0.f, 0.f };
+};
+
+} // namespace vrm::editor
