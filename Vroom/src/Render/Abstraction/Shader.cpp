86cec57be5e2c131d3997595a0c30984aba5ba07
Alexandre <beaujonalexandre@gmail.com>
2025-09-23 00:24:51 +0200

    BUGFIX: shader is reset after setting a new material to a pass material. Caused duplication of shaders code.

--- Vroom/src/Render/Abstraction/Shader.cpp
+++ Vroom/src/Render/Abstraction/Shader.cpp
@@ -1,44 +1,38 @@
 #include "Vroom/Render/Abstraction/Shader.h"
+#include <format>
 
-#include "Vroom/Render/Abstraction/GLCall.h"
 #include "Vroom/Core/Log.h"
-
-#include "Vroom/Render/Abstraction/Texture.h"
 #include "Vroom/Render/Abstraction/Buffer.h"
+#include "Vroom/Render/Abstraction/GLCall.h"
+#include "Vroom/Render/Abstraction/Texture.h"
 
 using namespace vrm;
 using namespace vrm::gl;
 
-Shader::Shader()
-{
-}
+Shader::Shader() {}
 
-Shader::~Shader()
-{
-  unload();
-}
+Shader::~Shader() { unload(); }
 
-glm::uvec3 Shader::ComputeDispatchSize(const glm::uvec3& localSize, const glm::uvec3& desiredMinInvocations)
+glm::uvec3 Shader::ComputeDispatchSize(const glm::uvec3& localSize,
+                                       const glm::uvec3& desiredMinInvocations)
 {
   return (desiredMinInvocations + localSize - glm::uvec3(1)) / localSize;
 }
 
-bool Shader::addShaderStage(const EShaderType& type, const GLString& source, bool checkErrors)
+bool Shader::addShaderStage(const EShaderType& type, const GLString& source,
+                            bool checkErrors)
 {
-  if (m_RendererID == 0)
-  {
-    m_RendererID = glCreateProgram();
-  }
+  if (m_RendererID == 0) { m_RendererID = glCreateProgram(); }
 
-  const static std::unordered_map<EShaderType, GLenum> table =
-  {
-    { EShaderType::eVertex, GL_VERTEX_SHADER },
+  const static std::unordered_map<EShaderType, GLenum> table = {
+    {   EShaderType::eVertex,   GL_VERTEX_SHADER },
     { EShaderType::eGeometry, GL_GEOMETRY_SHADER },
     { EShaderType::eFragment, GL_FRAGMENT_SHADER },
-    { EShaderType::eCompute, GL_COMPUTE_SHADER }
+    {  EShaderType::eCompute,  GL_COMPUTE_SHADER }
   };
 
-  VRM_CHECK_RET_FALSE_MSG(table.contains(type), "Unsupported shader type {}", static_cast<uint8_t>(type));
+  VRM_CHECK_RET_FALSE_MSG(table.contains(type), "Unsupported shader type {}",
+                          static_cast<uint8_t>(type));
   GLenum glType = table.at(type);
   GLuint shader = CompileShader(glType, source, checkErrors);
 
@@ -50,7 +44,8 @@ bool Shader::addShaderStage(const EShaderType& type, const GLString& source, boo
   return true;
 }
 
-GLuint Shader::CompileShader(GLenum type, const GLString& source, bool recordErrors)
+GLuint Shader::CompileShader(GLenum type, const GLString& source,
+                             bool recordErrors)
 {
   GLCall(GLuint id = glCreateShader(type));
   const GLchar* src = source.c_str();
@@ -81,7 +76,6 @@ GLuint Shader::CompileShader(GLenum type, const GLString& source, bool recordErr
 bool Shader::validate(bool checkErrors)
 {
   GLCall(glLinkProgram(m_RendererID));
-  GLCall(glValidateProgram(m_RendererID));
 
   GLint linkStatus;
   GLCall(glGetProgramiv(m_RendererID, GL_LINK_STATUS, &linkStatus));
@@ -94,13 +88,42 @@ bool Shader::validate(bool checkErrors)
 
       std::basic_string<GLchar> s(maxLength, ' ');
 
-      GLCall(glGetProgramInfoLog(m_RendererID, maxLength, &maxLength, s.data()));
+      GLCall(
+        glGetProgramInfoLog(m_RendererID, maxLength, &maxLength, s.data()));
+      m_errorRecord += std::format("[Link status {:x}]\n", linkStatus);
       m_errorRecord += s;
       m_errorRecord += " ";
     }
 
     GLCall(glDeleteProgram(m_RendererID));
     m_RendererID = 0;
+  }
+
+  if (m_RendererID)
+  {
+    GLCall(glValidateProgram(m_RendererID));
+    GLint validateStatus;
+    GLCall(glGetProgramiv(m_RendererID, GL_VALIDATE_STATUS, &validateStatus));
+    if (validateStatus == GL_FALSE)
+    {
+      if (checkErrors)
+      {
+        GLint maxLength = 0;
+        GLCall(glGetProgramiv(m_RendererID, GL_INFO_LOG_LENGTH, &maxLength));
+
+        std::basic_string<GLchar> s(maxLength, ' ');
+
+        GLCall(
+          glGetProgramInfoLog(m_RendererID, maxLength, &maxLength, s.data()));
+        m_errorRecord +=
+          std::format("[Validate status {:x}]\n", validateStatus);
+        m_errorRecord += s;
+        m_errorRecord += " ";
+      }
+
+      GLCall(glDeleteProgram(m_RendererID));
+      m_RendererID = 0;
+    }
   }
 
   for (const auto& shader : m_attachedShaders)
@@ -114,7 +137,7 @@ bool Shader::validate(bool checkErrors)
 
 Shader::GLString Shader::getError()
 {
-  auto error = std::move(m_errorRecord);
+  auto error    = std::move(m_errorRecord);
   m_errorRecord = "";
   return std::move(error);
 }
@@ -132,22 +155,17 @@ void Shader::unload()
   }
 }
 
-void Shader::bind() const
-{
-  GLCall(glUseProgram(m_RendererID));
-}
+void Shader::bind() const { GLCall(glUseProgram(m_RendererID)); }
 
-void Shader::unbind() const
-{
-  GLCall(glUseProgram(0));
-}
+void Shader::unbind() const { GLCall(glUseProgram(0)); }
 
 void Shader::setUniform1i(const GLString& name, int value) const
 {
   GLCall(glUniform1i(getUniformLocation(name), value));
 }
 
-void Shader::setUniform1iv(const GLString& name, int count, const int* value) const
+void Shader::setUniform1iv(const GLString& name, int count,
+                           const int* value) const
 {
   GLCall(glUniform1iv(getUniformLocation(name), count, value));
 }
@@ -157,7 +175,8 @@ void Shader::setUniform1ui(const GLString& name, unsigned int value) const
   GLCall(glUniform1ui(getUniformLocation(name), value));
 }
 
-void Shader::setUniform2ui(const GLString& name, unsigned int v0, unsigned int v1) const
+void Shader::setUniform2ui(const GLString& name, unsigned int v0,
+                           unsigned int v1) const
 {
   GLCall(glUniform2ui(getUniformLocation(name), v0, v1));
 }
@@ -172,7 +191,8 @@ void Shader::setUniform2f(const GLString& name, float v0, float v1) const
   GLCall(glUniform2f(getUniformLocation(name), v0, v1));
 }
 
-void Shader::setUniform3f(const GLString& name, float v0, float v1, float v2) const
+void Shader::setUniform3f(const GLString& name, float v0, float v1,
+                          float v2) const
 {
   GLCall(glUniform3f(getUniformLocation(name), v0, v1, v2));
 }
@@ -182,7 +202,8 @@ void Shader::setUniform3f(const GLString& name, const glm::vec3& vec) const
   setUniform3f(name, vec.x, vec.y, vec.z);
 }
 
-void Shader::setUniform4f(const GLString& name, float v0, float v1, float v2, float v3) const
+void Shader::setUniform4f(const GLString& name, float v0, float v1, float v2,
+                          float v3) const
 {
   GLCall(glUniform4f(getUniformLocation(name), v0, v1, v2, v3));
 }
@@ -209,7 +230,8 @@ GLuint Shader::getStorageBufferLocation(const GLString& name) const
   if (!m_ssboIndexCache.contains(name))
   {
     SsboIndexLoc indexLoc;
-    indexLoc.index = glGetProgramResourceIndex(m_RendererID, GL_SHADER_STORAGE_BLOCK, name.c_str());
+    indexLoc.index = glGetProgramResourceIndex(
+      m_RendererID, GL_SHADER_STORAGE_BLOCK, name.c_str());
     if (indexLoc.index != GL_INVALID_INDEX)
     {
       indexLoc.loc = m_nextSsboLoc++;
@@ -218,7 +240,9 @@ GLuint Shader::getStorageBufferLocation(const GLString& name) const
     else
     {
       indexLoc.loc = -1;
-      VRM_LOG_ERROR("Could not find StorageBuffer \"{}\" in shader. Maybe it is dead code ?", name);
+      VRM_LOG_ERROR("Could not find StorageBuffer \"{}\" in shader. Maybe it "
+                    "is dead code ?",
+                    name);
     }
 
     m_ssboIndexCache[name] = indexLoc;
@@ -227,14 +251,16 @@ GLuint Shader::getStorageBufferLocation(const GLString& name) const
   return m_ssboIndexCache.at(name).loc;
 }
 
-void Shader::setTexture(const GLString &name, const Texture& texture, GLuint slot) const
+void Shader::setTexture(const GLString& name, const Texture& texture,
+                        GLuint slot) const
 {
   glActiveTexture(GL_TEXTURE0 + slot);
   texture.bind();
   setUniform1i(name, slot);
 }
 
-void Shader::setStorageBuffer(const GLString& name, const gl::Buffer& ssbo) const
+void Shader::setStorageBuffer(const GLString&   name,
+                              const gl::Buffer& ssbo) const
 {
   GLuint loc = getStorageBufferLocation(name);
   if (loc != -1)
@@ -244,13 +270,15 @@ void Shader::setStorageBuffer(const GLString& name, const gl::Buffer& ssbo) cons
   }
 }
 
-void Shader::setStorageBuffer(const GLString &name, const gl::Buffer &ssbo, GLintptr offset, GLintptr size) const
+void Shader::setStorageBuffer(const GLString& name, const gl::Buffer& ssbo,
+                              GLintptr offset, GLintptr size) const
 {
   GLuint loc = getStorageBufferLocation(name);
   if (loc != -1)
   {
     Buffer::Bind(ssbo, GL_SHADER_STORAGE_BUFFER);
     GLCall(glBindBufferBase(GL_SHADER_STORAGE_BUFFER, loc, ssbo.getRenderId()));
-    glBindBufferRange(GL_SHADER_STORAGE_BUFFER, loc, ssbo.getRenderId(), offset, size);
+    glBindBufferRange(GL_SHADER_STORAGE_BUFFER, loc, ssbo.getRenderId(), offset,
+                      size);
   }
 }
--- Vroom/src/Render/PassMaterials.cpp
+++ Vroom/src/Render/PassMaterials.cpp
@@ -7,56 +7,58 @@ using namespace vrm;
 
 // PassMaterial
 
-PassMaterial::PassMaterial()
-{
-}
+PassMaterial::PassMaterial() {}
 
-PassMaterial::~PassMaterial()
-{
-}
+PassMaterial::~PassMaterial() {}
 
 void PassMaterial::setMaterialAsset(MaterialAsset::Handle material)
 {
   m_materialAsset = material;
+  m_gpuShader.unload();
   m_needsPrepare = true;
 }
 
-bool PassMaterial::prepare(const MaterialDefines &defines)
+bool PassMaterial::prepare(const MaterialDefines& defines)
 {
-  AssetManager &manager = AssetManager::Get();
+  AssetManager& manager = AssetManager::Get();
 
   ShaderData shaderData = m_materialAsset->getShaderData();
 
   for (const auto& [define, value] : defines)
   {
-    shaderData.addDefine(ShaderData::EShaderType::eAll, ShaderData::Define{ define, value });
+    shaderData.addDefine(ShaderData::EShaderType::eAll,
+                         ShaderData::Define{ define, value });
   }
 
   ShaderData::FullShader rawShaderData = shaderData.combine();
 
-  static const std::unordered_map<ShaderData::EShaderType, gl::Shader::EShaderType> s_convertTable =
-  {
-    { ShaderData::EShaderType::eVertex, gl::Shader::EShaderType::eVertex },
-    { ShaderData::EShaderType::eGeometry, gl::Shader::EShaderType::eGeometry },
-    { ShaderData::EShaderType::eFragment, gl::Shader::EShaderType::eFragment },
-    { ShaderData::EShaderType::eCompute, gl::Shader::EShaderType::eCompute },
+  static const std::unordered_map<ShaderData::EShaderType,
+                                  gl::Shader::EShaderType>
+    s_convertTable = {
+      {   ShaderData::EShaderType::eVertex,gl::Shader::EShaderType::eVertex                                           },
+      { ShaderData::EShaderType::eGeometry,
+       gl::Shader::EShaderType::eGeometry                                    },
+      { ShaderData::EShaderType::eFragment,
+       gl::Shader::EShaderType::eFragment                                    },
+      {  ShaderData::EShaderType::eCompute, gl::Shader::EShaderType::eCompute },
   };
 
   for (size_t i = 0; i < (size_t)ShaderData::EShaderType::eCount; ++i)
   {
-    auto stage = ShaderData::EShaderType(i);
-    const std::string& src = rawShaderData[stage];
+    auto               stage = ShaderData::EShaderType(i);
+    const std::string& src   = rawShaderData[stage];
     if (src.size() > 0)
     {
       VRM_CHECK_RET_FALSE_MSG(
         m_gpuShader.addShaderStage(s_convertTable.at(stage), src, true),
-        "With shader:\n{}\n\nCould not compile material shader. Error log:\n{}", src, m_gpuShader.getError());
+        "With shader:\n{}\n\nCould not compile material shader. Error log:\n{}",
+        src, m_gpuShader.getError());
     }
   }
 
-  VRM_ASSERT_MSG(
-      m_gpuShader.validate(true),
-      "Could not validate material shader. Error log:\n{}", m_gpuShader.getError());
+  VRM_ASSERT_MSG(m_gpuShader.validate(true),
+                 "Could not validate material shader. Error log:\n{}",
+                 m_gpuShader.getError());
 
   // std::string dumpPath = getFilePath() + ".dump.glsl";
   // rawShaderData.fragment.dump(dumpPath);
@@ -68,39 +70,25 @@ bool PassMaterial::prepare(const MaterialDefines &defines)
 
 // PassMaterials
 
-PassMaterials::PassMaterials()
-{
+PassMaterials::PassMaterials() {}
 
-}
-
-PassMaterials::~PassMaterials()
-{
-
-}
+PassMaterials::~PassMaterials() {}
 
 const PassMaterial& PassMaterials::getMaterial(const Key& key)
 {
-  auto [it, inserted] = m_materials.try_emplace(key);
+  auto [it, inserted]    = m_materials.try_emplace(key);
   PassMaterial& material = it->second;
 
   if (inserted)
   {
     material.setMaterialAsset(key.asset);
-    if (key.defines)
-      material.prepare(*key.defines);
-    else
-      material.prepare({});
+    if (key.defines) material.prepare(*key.defines);
+    else material.prepare({});
   }
-  
+
   return material;
 }
 
-void PassMaterials::releaseMaterial(const Key& key)
-{
-  m_materials.erase(key);
-}
+void PassMaterials::releaseMaterial(const Key& key) { m_materials.erase(key); }
 
-void PassMaterials::clear()
-{
-  m_materials.clear();
-}
+void PassMaterials::clear() { m_materials.clear(); }
